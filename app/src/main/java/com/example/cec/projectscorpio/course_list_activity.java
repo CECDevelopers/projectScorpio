bc0cd127b1c04b706c915ae601b82613ccc24fa1
Swaroop <swarup.e1998@gmail.com>
2017-10-08 16:48:40 +0530

    Created new layout with List of Coursers.

---
+++ app/src/main/java/com/example/cec/projectscorpio/course_list_activity.java
@@ -0,0 +1,16 @@
+package com.example.cec.projectscorpio;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 007 on 10/8/2017.
+ */
+
+public class course_list_activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cec_courses_list);
+    }
+}
