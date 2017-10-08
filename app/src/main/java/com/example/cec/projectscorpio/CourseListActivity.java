f803fda9fa2259688ca470ebadf0e950a3de8855
Pavan Hegde <pavanrkadave@gmail.com>
2017-10-08 12:50:44 +0530

    Changes to CourseListActivity to handle the click event.

--- app/src/main/java/com/example/cec/projectscorpio/CourseListActivity.java
+++ app/src/main/java/com/example/cec/projectscorpio/CourseListActivity.java
@@ -2,15 +2,49 @@ package com.example.cec.projectscorpio;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 /**
  * Created by 007 on 10/8/2017.
  */
 
 public class CourseListActivity extends AppCompatActivity {
+
+    ListView courseView;
+
+    ArrayList<String> courseList = new ArrayList<String>();
+
+    ArrayAdapter courseAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cec_courses_list);
+
+        courseList.add(getString(R.string.CS));
+        courseList.add(getString(R.string.CE));
+        courseList.add(getString(R.string.EC));
+        courseList.add(getString(R.string.ME));
+
+        courseView = (ListView) findViewById(R.id.list);
+
+        courseAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, courseList);
+
+        courseView.setAdapter(courseAdapter);
+
+        courseView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(), "You Clicked " +
+                        adapterView.getItemAtPosition(i), Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
