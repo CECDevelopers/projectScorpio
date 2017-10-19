7cf4005fb8756dcee6bcb95d973fdfaac1b75dfe
Swaroop <swarup.e1998@gmail.com>
2017-10-19 12:24:30 +0530

    Added Subjects List Activities for each semesters in Computer Science branch.

--- app/src/main/java/com/example/cec/projectscorpio/branches/CSESemesterListActivity.java
+++ app/src/main/java/com/example/cec/projectscorpio/branches/CSESemesterListActivity.java
@@ -9,7 +9,12 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.example.cec.projectscorpio.R;
-import com.example.cec.projectscorpio.YoutubeActivity;
+import com.example.cec.projectscorpio.branches.computerScience.EighthSemCSSubjectsListActivity;
+import com.example.cec.projectscorpio.branches.computerScience.FifthSemCSSubjectsListActivity;
+import com.example.cec.projectscorpio.branches.computerScience.FourthSemCSSubjectsListActivity;
+import com.example.cec.projectscorpio.branches.computerScience.SeventhSemCSSubjectsListActivity;
+import com.example.cec.projectscorpio.branches.computerScience.SixthSemCSSubjectsListActivity;
+import com.example.cec.projectscorpio.branches.computerScience.ThirdSemCSSubjectsListActivity;
 
 import java.util.ArrayList;
 
@@ -45,8 +50,28 @@ public class CSESemesterListActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 if( i == 0 ) {
-                    Intent intent = new Intent(CSESemesterListActivity.this, YoutubeActivity.class);
+                    Intent intent = new Intent(CSESemesterListActivity.this, ThirdSemCSSubjectsListActivity.class);
                     startActivityForResult(intent, 0);
+                }
+                if( i == 1 ) {
+                    Intent intent = new Intent(CSESemesterListActivity.this, FourthSemCSSubjectsListActivity.class);
+                    startActivityForResult(intent, 1);
+                }
+                if( i == 2 ) {
+                    Intent intent = new Intent(CSESemesterListActivity.this, FifthSemCSSubjectsListActivity.class);
+                    startActivityForResult(intent, 2);
+                }
+                if( i == 3 ) {
+                    Intent intent = new Intent(CSESemesterListActivity.this, SixthSemCSSubjectsListActivity.class);
+                    startActivityForResult(intent, 3);
+                }
+                if( i == 4 ) {
+                    Intent intent = new Intent(CSESemesterListActivity.this, SeventhSemCSSubjectsListActivity.class);
+                    startActivityForResult(intent, 4);
+                }
+                if( i == 5 ) {
+                    Intent intent = new Intent(CSESemesterListActivity.this, EighthSemCSSubjectsListActivity.class);
+                    startActivityForResult(intent, 5);
                 }
 
             }
---
+++ app/src/main/java/com/example/cec/projectscorpio/branches/computerScience/FourthSemCSSubjectsListActivity.java
@@ -0,0 +1,53 @@
+package com.example.cec.projectscorpio.branches.computerScience;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.example.cec.projectscorpio.R;
+import com.example.cec.projectscorpio.YoutubeActivity;
+
+import java.util.ArrayList;
+
+
+
+public class FourthSemCSSubjectsListActivity extends AppCompatActivity {
+
+    ListView subjectsView;
+
+    ArrayList<String> subjectsList = new ArrayList<String>();
+
+    ArrayAdapter subjectsAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.subjects_list);
+
+        subjectsList.add("Subject 1");
+        subjectsList.add("Subject 2");
+        subjectsList.add("Subject 3");
+        subjectsList.add("Subject 4");
+        subjectsList.add("Subject 5");
+        subjectsList.add("Subject 6");
+
+        subjectsView = (ListView) findViewById(R.id.subjects_list);
+
+        subjectsAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, subjectsList);
+
+        subjectsView.setAdapter(subjectsAdapter);
+
+        subjectsView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(FourthSemCSSubjectsListActivity.this, YoutubeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
